34b2a8bea1d18fb9517867d919972075862d1213
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-07-14 18:03:19 -0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,7 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+card_type = ["ace", "spears", "dimonds","hearts"]
+first_card_number = random.choice(cards)
+first_card_type = random.choice(card_type)
+print ("you got a ", first_card_number ,"of", first_card_type)
+
